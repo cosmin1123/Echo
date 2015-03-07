b09506b0b29dff49cd46d9c8595dd707fd772447
Cătălin Ramaşcanu <catalin.ramascanu@cti.pub.ro>
2015-03-07 17:08:31 +0200

    Added index.html + jquery mobile

--- src/com/Echo/Echo.java
+++ src/com/Echo/Echo.java
@@ -1,7 +1,12 @@
 package com.Echo;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.Echo.R;
 
 public class Echo extends Activity {
@@ -12,5 +17,25 @@ public class Echo extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl("file:///android_asset/web/index.html");
+
+//        WebSettings sett = webView.getSettings();
+//        sett.setAllowUniversalAccessFromFileURLs(true);
+//        sett.setAllowFileAccessFromFileURLs(true);
+
+
+        PackageManager m = getPackageManager();
+        String s = getPackageName();
+        PackageInfo p = null;
+        try {
+            p = m.getPackageInfo(s, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        s = p.applicationInfo.dataDir;
+
+        System.out.println(s);
     }
 }
