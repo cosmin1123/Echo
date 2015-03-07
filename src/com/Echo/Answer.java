79f49bdbecdb4b13c0b5c6bd5225cbc062b88b25
cosmin1123 <oiper1123>
2015-03-07 20:18:29 +0200

    added initial Screen

---
+++ src/com/Echo/Answer.java
@@ -0,0 +1,16 @@
+package com.Echo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.Echo.R;
+
+/**
+ * Created by Cosmin on 3/7/2015.
+ */
+public class Answer  extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.answer);
+    }
+}
--- src/com/Echo/Echo.java
+++ src/com/Echo/Echo.java
@@ -1,23 +1,61 @@
 package com.Echo;
 
 import android.app.Activity;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.os.Build;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import com.Echo.R;
+
 
 public class Echo extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+    public void setButtonListener() {
+        Button butt1 = (Button) findViewById(R.id.answerButton);
+        Button butt2 = (Button) findViewById(R.id.askButton);
+
+        butt1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i=new Intent(
+                        Echo.this,
+                        Answer.class);
+
+                startActivity(i);
+            }
+        });
+
+        butt2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i=new Intent(
+                        Echo.this,
+                        Answer.class);
+
+                startActivity(i);
+            }
+        });
+    }
+
+    public void setFont() {
+        Button butt1 = (Button) findViewById(R.id.answerButton);
+        Button butt2 = (Button) findViewById(R.id.askButton);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Goffik-O.ttf");
+        butt1.setTypeface(font);
+        butt2.setTypeface(font);
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        setFont();
+        setButtonListener();
 
 
     }
