2a23be52d9bdd220052c21381acbb00676fc99fe
Cătălin Ramaşcanu <catalin.ramascanu@cti.pub.ro>
2015-03-07 18:15:35 +0200

    deleteeee

--- src/com/Echo/Echo.java
+++ src/com/Echo/Echo.java
@@ -19,25 +19,6 @@ public class Echo extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        WebView webView = (WebView) findViewById(R.id.webview);
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setDomStorageEnabled(true);
-        webView.getSettings().setLoadsImagesAutomatically(true);
 
-        webView.loadUrl("file:///android_asset/web/index.html");
-
-
-        PackageManager m = getPackageManager();
-        String s = getPackageName();
-        PackageInfo p = null;
-        try {
-            p = m.getPackageInfo(s, 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        s = p.applicationInfo.dataDir;
-
-        System.out.println(s);
     }
 }
